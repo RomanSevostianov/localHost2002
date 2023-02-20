0080197e24635bde065be4798e6c691806a0e0e6
Роман <pangujailbreake58@gmail.com>
2023-02-20 15:50:44 +0300

    1

---
+++ src/main/java/com/example/local2002/Local2002Application.java
@@ -0,0 +1,13 @@
+package com.example.local2002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Local2002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Local2002Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/local2002/Local2002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.local2002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Local2002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
